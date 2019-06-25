378653f5f1f1779df4bd212d638b92cecbe015b2
Tania Cáceres <taniacaceres@gmail.com>
2019-06-25 17:58:10 -0400

    Updates

---
+++ precache-manifest.5b4b0309e78d8927426ff1347a57a288.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf3ef349207dfedd89a63135dbc0ed5",
+    "url": "/bi-react-practice/index.html"
+  },
+  {
+    "revision": "655d847263326f555569",
+    "url": "/bi-react-practice/static/css/main.be0f1c08.chunk.css"
+  },
+  {
+    "revision": "1b6d7c1c8216c7c9c458",
+    "url": "/bi-react-practice/static/js/2.1427ce24.chunk.js"
+  },
+  {
+    "revision": "655d847263326f555569",
+    "url": "/bi-react-practice/static/js/main.f7ac37e9.chunk.js"
+  },
+  {
+    "revision": "d87318d415d0bd00ba97",
+    "url": "/bi-react-practice/static/js/runtime~main.52b4407c.js"
+  }
+]);
--- src/redux/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import RMReducer from './RMReducer'
-import Users from './Users'
-
-export default combineReducers({
-  rickAndMorty: RMReducer,
-  users: Users
-})
